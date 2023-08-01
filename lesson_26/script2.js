01f8e5c4aa1677cd4fbf959dd725bd598455cc37
MasiukBeTech <i0631097180@gmail.com>
2023-08-02 00:37:14 +0300

    HW26_2

--- lesson_26/script2.js
+++ lesson_26/script2.js
@@ -3,21 +3,22 @@ function Accumulator(number) {
 }
 
 Accumulator.prototype.increment = function() {
-    console.log(this.value += 1);
+    console.log(this.value + 1);
 }
 Accumulator.prototype.decrement = function() {
-    console.log(this.value -= 1);
+    console.log(this.value - 1);
 }
 
 function CancelableAccumulator(number) {
     Accumulator.call(this, number);
 }
-CancelableAccumulator.prototype.clear = function() {
-    this.value = 0
-    console.log(this.value);
-}
 
 CancelableAccumulator.prototype = Accumulator.prototype;
+
+CancelableAccumulator.prototype.clear = function() {
+    // this.value = null;
+    console.log(this.value);
+}
 
 this.number = +prompt('Please, enter number');
 
@@ -26,4 +27,6 @@ let num = new CancelableAccumulator(this.number);
 num.increment();
 num.decrement();
 num.clear();
+
+
 
