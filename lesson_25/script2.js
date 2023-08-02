460f49895bddd21fbdc51ce7a3859151a9c5636f
MasiukBeTech <i0631097180@gmail.com>
2023-08-02 23:16:01 +0300

    fixedComment

--- lesson_25/script2.js
+++ lesson_25/script2.js
@@ -37,27 +37,28 @@ user.sayHello();
 let culculator = {
     number1: null,
     number2: null,
+    num1: 0,
+    num2: 0,
     ask: function() {
-        this.number1 = prompt('Please, enter first numbers');
-        this.number2 = prompt('Please, enter second numbers');
+        this.number1 = +prompt('Please, enter first numbers');
+        this.number2 = +prompt('Please, enter second numbers');
+
+        this.num1 = this.number1;
+        this.num2 = this.number2;
     },
     sum: function() {
-        let num1 = parseFloat(this.number1);
-        let num2 = parseFloat(this.number2);
     
-        if (!isNaN(num1) && !isNaN(num2)) {
-          let resultSum = num1 + num2;
+        if (!isNaN(this.num1) && !isNaN(this.num2)) {
+          let resultSum = this.num1 + this.num2;
           alert(resultSum);
         } else {
           alert('Invalid input!');
         }
     },
     mul: function() {
-        let num1 = parseFloat(this.number1);
-        let num2 = parseFloat(this.number2);
     
-        if (!isNaN(num1) && !isNaN(num2)) {
-          let resultMultiplication = num1 * num2;
+        if (!isNaN(this.num1) && !isNaN(this.num2)) {
+          let resultMultiplication = this.num1 * this.num2;
           alert(resultMultiplication);
         } else {
           alert('Invalid input!');
